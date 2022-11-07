0e970f9a93270ec7aef9c3599bc752c63c7382dc
Yuriy Kazak <yuriymilax@icloud.com>
2022-11-07 10:54:34 +0300

    Added raylib cpp wrapper

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,44 @@
-#include <raylib.h>
+#include <raylib-cpp.hpp>
+#include <imgui.h>
+#include <string>
 
 #include "imgui/imgui_impl_raylib.h"
-#include "imgui.h"
 
 int main() {
-    InitWindow(1280, 720, "App Template");
-    SetTargetFPS(75);
-
+    auto window = raylib::Window(1280, 720, "App Templae");
+    window.SetTargetFPS(75);
     rlImGuiSetup();
 
-    while(!WindowShouldClose())
-    {
-        BeginDrawing();
-        ClearBackground(RAYWHITE);
-        DrawText("Hello :)",
-                 0,
-                 0,
-                 24, BLACK);
-        rlImGuiBegin();
-        ImGui::ShowDemoWindow();
-        rlImGuiEnd();
-        EndDrawing();
-    }
+    std::string text = "Everything works fine :)";
+    int textSize = 24;
+    float color[3] = {
+            0, 0, 0
+    };
 
-    rlImGuiShutdown();
-    CloseWindow();
+    while(!window.ShouldClose())
+    {
+        window.BeginDrawing();
+        window.ClearBackground(RAYWHITE);
+        raylib::DrawText(text,
+                         0, 0,
+                         textSize,
+                         {
+                                 static_cast<unsigned char>(color[0] * 255),
+                                 static_cast<unsigned char>(color[1] * 255),
+                                 static_cast<unsigned char>(color[2] * 255),
+                                 255
+                         });
+        rlImGuiBegin();
+        ImGui::Begin("Settings");
+        {
+            ImGui::InputText("Text", text.data(), 128);
+            ImGui::ColorEdit3("Text color", &color[0]);
+            ImGui::DragInt("Text size", &textSize,
+                           1, 1, 128);
+            ImGui::End();
+        }
+        rlImGuiEnd();
+        window.EndDrawing();
+    }
 	return 0;
 }
